3962964787387fbc330091404643d644205b7659
johnzieman <j.g.zokirov@gmail.com>
2021-12-03 12:23:52 +0500

    images

--- app/src/main/java/com/ziemapp/johnzieman/flickrgallery/ThumbnailDownloader.kt
+++ app/src/main/java/com/ziemapp/johnzieman/flickrgallery/ThumbnailDownloader.kt
@@ -13,10 +13,11 @@ import java.util.concurrent.ConcurrentHashMap
 
 private const val TAG = "ThumbnailDownloader"
 private const val MESSAGE_DOWNLOAD = 0
+
 class ThumbnailDownloader<in T>(
     private val responseHandler: Handler,
     private val onThumbnailDownloaded: (T, Bitmap) -> Unit
-): HandlerThread(TAG), LifecycleObserver {
+) : HandlerThread(TAG), LifecycleObserver {
     private var hasQuit = false
     private lateinit var requestHandler: Handler
     private val requestMap = ConcurrentHashMap<T, String>()
@@ -28,7 +29,7 @@ class ThumbnailDownloader<in T>(
         return super.quit()
     }
 
-    fun queueThumbnail(target: T, url: String){
+    fun queueThumbnail(target: T, url: String) {
         Log.d(TAG, "Got a URl: $url")
         requestMap[target] = url
         requestHandler.obtainMessage(MESSAGE_DOWNLOAD, target)
@@ -40,7 +41,8 @@ class ThumbnailDownloader<in T>(
     override fun onLooperPrepared() {
         requestHandler = object : Handler() {
             override fun handleMessage(msg: Message) {
-                if (msg.what == MESSAGE_DOWNLOAD) {val target = msg.obj as T
+                if (msg.what == MESSAGE_DOWNLOAD) {
+                    val target = msg.obj as T
                     Log.i(TAG, "Got a request for URL: ${requestMap[target]}")
                     handleRequest(target)
                 }
@@ -51,6 +53,14 @@ class ThumbnailDownloader<in T>(
     private fun handleRequest(target: T) {
         val url = requestMap[target] ?: return
         val bitmap = flickrFetchr.fetchPhoto(url) ?: return
+        responseHandler.post(Runnable {
+            if (requestMap[target] != url || hasQuit) {
+                return@Runnable
+            }
+            requestMap.remove(target)
+            onThumbnailDownloaded(target, bitmap)
+
+        })
     }
 
     @OnLifecycleEvent(Lifecycle.Event.ON_CREATE)
@@ -58,6 +68,7 @@ class ThumbnailDownloader<in T>(
         start()
         looper
     }
+
     @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
     fun tearDown() {
         quit()
